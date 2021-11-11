bd71d30bf95c7a2151dd5ea0cf7283f774f263dd
HassanChaabawi <91587945+HassanChaabawi@users.noreply.github.com>
2021-11-11 17:43:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import './App.css';
-import Main from './components/Main';
+import { useState } from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    let id = 1;
+    if(todos.length > 0) {
+      id = todos[0].id + 1
+    }
+    let todo = {id: id, text: text, completed: false, important: false}
+    let newTodos = [todo, ...todos]
+    setTodos(newTodos)
+  };
+
+  const removeTodo = (id) => {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+  const importantTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if(todo.id === id) {
+        todo.important = !todo.important
+      }
+      return todo
+    })
+
+    setTodos(updatedTodos)
+  }
+  let sortedTodos = todos.sort((a, b) => b.important - a.important)
   return (
-    <div className='todo-app'>
-      <Main />
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <hr className="seperator"/>
+      {sortedTodos.map((todo) => {
+        return (
+          <TodoItem removeTodo={removeTodo} completeTodo={completeTodo} importantTodo={importantTodo} todo={todo} key={todo.id}/>
+        )
+      })}
     </div>
   );
 }
